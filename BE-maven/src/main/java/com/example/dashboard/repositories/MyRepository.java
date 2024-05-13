eaf64bad1d7d652222a36cc86a4bcfdcbb9222eb
Simone <saggese2001@gmail.com>
2024-05-13 12:48:21 +0200

    elimino backend non utilizzati

--- BE-maven/src/main/java/com/example/dashboard/repositories/MyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dashboard.repositories;
-
-import com.example.dashboard.models.MyTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-/* Classe che definisce il repository (database)  */
-public interface MyRepository extends JpaRepository<MyTable, Long> {
-
-}

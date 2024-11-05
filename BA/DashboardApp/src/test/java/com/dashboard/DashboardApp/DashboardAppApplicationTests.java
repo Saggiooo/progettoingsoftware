f280851ddbededa8d1fc23be1711dc1f1ffd102a
Simone <saggese2001@gmail.com>
2024-11-05 17:21:19 +0100

    modifiche 2

---
+++ BA/DashboardApp/src/test/java/com/dashboard/DashboardApp/DashboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.DashboardApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
